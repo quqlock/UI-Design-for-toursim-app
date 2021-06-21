7eeeb5709d8c855fe4a1e4ad1d65c0f035f632d1
Krzysztof Niedbala <quqlock@gmail.com>
2021-06-21 23:06:11 +0200

    2 screens

--- lib/dest_screen.dart
+++ lib/dest_screen.dart
@@ -12,34 +12,54 @@ class DestScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Stack(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Image.asset(
-                            dest.destImg,
-                            fit: BoxFit.fitWidth,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Stack(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                              dest.destImg,
+                              fit: BoxFit.fitWidth,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Positioned(
+                        top: 15,
+                        left: 15,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                            width: 45,
+                            height: 45,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.blue[200],
+                            ),
+                            child: Center(
+                              child: Icon(
+                                Icons.arrow_back_ios_outlined,
+                                color: Colors.white,
+                              ),
+                            ),
                           ),
                         ),
-                      ],
-                    ),
-                    Positioned(
-                      top: 15,
-                      left: 15,
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
+                      ),
+                      Positioned(
+                        top: 15,
+                        right: 15,
                         child: Container(
                           width: 45,
                           height: 45,
@@ -49,125 +69,219 @@ class DestScreen extends StatelessWidget {
                           ),
                           child: Center(
                             child: Icon(
-                              Icons.arrow_back_ios,
+                              Icons.share,
                               color: Colors.white,
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Positioned(
-                      top: 15,
-                      right: 15,
-                      child: Container(
-                        width: 45,
-                        height: 45,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.blue[200],
-                        ),
-                        child: Center(
-                          child: Icon(
-                            Icons.share,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 70,
-                      left: 15,
-                      child: Container(
-                        width: 200,
-                        child: Text(
-                          dest.destName,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 33,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 30,
-                      left: 15,
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.baseline,
-                        textBaseline: TextBaseline.alphabetic,
-                        children: [
-                          Text(
-                            '\$' + dest.destPrice.toStringAsFixed(0),
+                      Positioned(
+                        bottom: 70,
+                        left: 15,
+                        child: Container(
+                          width: 200,
+                          child: Text(
+                            dest.destName,
                             style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 25,
                               fontWeight: FontWeight.bold,
+                              fontSize: 33,
+                              color: Colors.white,
                             ),
                           ),
-                          Text(
-                            '/person',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 30,
-                      right: 15,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(18),
-                          color: Colors.grey.withOpacity(0.7),
                         ),
-                        width: 85,
-                        height: 45,
+                      ),
+                      Positioned(
+                        bottom: 30,
+                        left: 15,
                         child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.baseline,
+                          textBaseline: TextBaseline.alphabetic,
                           children: [
-                            Icon(
-                              Icons.star,
-                              color: Colors.amber,
-                            ),
-                            SizedBox(
-                              width: 2,
-                            ),
                             Text(
-                              '4.9',
+                              '\$' + dest.destPrice.toStringAsFixed(0),
                               style: TextStyle(
                                 color: Colors.white,
+                                fontSize: 25,
                                 fontWeight: FontWeight.bold,
-                                fontSize: 18,
                               ),
+                            ),
+                            Text(
+                              '/person',
+                              style: TextStyle(color: Colors.white),
                             ),
                           ],
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    top: 10.0,
-                    bottom: 10.0,
+                      Positioned(
+                        bottom: 30,
+                        right: 15,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(18),
+                            color: Colors.grey.withOpacity(0.7),
+                          ),
+                          width: 85,
+                          height: 45,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.star,
+                                color: Colors.amber,
+                              ),
+                              SizedBox(
+                                width: 2,
+                              ),
+                              Text(
+                                '4.9',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  child: Text(
-                    'About',
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 10.0,
+                      bottom: 10.0,
+                    ),
+                    child: Text(
+                      'About',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 23,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    dest.destDescription,
                     style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 23,
+                      color: Colors.grey,
+                      fontSize: 16,
+                      height: 1.5,
                     ),
                   ),
-                ),
-                Text(
-                  dest.destDescription,
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 16,
-                    height: 1.5,
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 20.0,
+                      bottom: 15.0,
+                    ),
+                    child: Text(
+                      'Gallery',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 23,
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 70,
+                        height: 70,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: Image.asset(
+                            dest.destImg,
+                            fit: BoxFit.fitWidth,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 70,
+                        height: 70,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: Image.asset(
+                            dest.destImg,
+                            fit: BoxFit.fitWidth,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 70,
+                        height: 70,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: Image.asset(
+                            dest.destImg,
+                            fit: BoxFit.fitWidth,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 70,
+                        height: 70,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: Image.asset(
+                            dest.destImg,
+                            fit: BoxFit.fitWidth,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints.tightFor(
+                        width: MediaQuery.of(context).size.width,
+                        height: 80,
+                      ),
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Spacer(),
+                              Text(
+                                'Book now',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Spacer(),
+                              Icon(Icons.arrow_forward),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
