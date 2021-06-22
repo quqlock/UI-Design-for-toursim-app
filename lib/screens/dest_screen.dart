52a9d66c13a3afb5b13bd2ecdf6fcfeec5d861f4
Quqlock <quqlock@gmail.com>
2021-06-22 08:08:38 +0200

    Refactoring DestScreen

--- lib/screens/dest_screen.dart
+++ lib/screens/dest_screen.dart
@@ -79,48 +79,51 @@ class DestScreen extends StatelessWidget {
 
   Widget buildGalleryWidget() {
     Widget _buildGalleryPhoto(bool extraPhotos) {
-      return Container(
-        width: 70,
-        height: 70,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(
-                    dest.destImg,
+      return Padding(
+        padding: const EdgeInsets.only(top: 10.0),
+        child: Container(
+          width: 70,
+          height: 70,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      dest.destImg,
+                    ),
                   ),
                 ),
               ),
-            ),
-            extraPhotos
-                ? Container(
-                    decoration: BoxDecoration(
-                      color: Colors.black.withOpacity(0.5),
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                  )
-                : Container(),
-            extraPhotos
-                ? Positioned(
-                    child: Center(
-                      child: Text(
-                        '+6',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+              extraPhotos
+                  ? Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    )
+                  : Container(),
+              extraPhotos
+                  ? Positioned(
+                      child: Center(
+                        child: Text(
+                          '+6',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
-                  )
-                : Container(),
-          ],
+                    )
+                  : Container(),
+            ],
+          ),
         ),
       );
     }
@@ -148,27 +151,48 @@ class DestScreen extends StatelessWidget {
   }
 
   Widget buildDestPhotoWidget(BuildContext context) {
-    return Stack(
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset(
-                dest.destImg,
-                fit: BoxFit.fitWidth,
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset(
+                  dest.destImg,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            top: 15,
+            left: 15,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Container(
+                width: 45,
+                height: 45,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.blue[200],
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.arrow_back_ios_outlined,
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ),
-          ],
-        ),
-        Positioned(
-          top: 15,
-          left: 15,
-          child: GestureDetector(
-            onTap: () {
-              Navigator.pop(context);
-            },
+          ),
+          Positioned(
+            top: 15,
+            right: 15,
             child: Container(
               width: 45,
               height: 45,
@@ -178,102 +202,84 @@ class DestScreen extends StatelessWidget {
               ),
               child: Center(
                 child: Icon(
-                  Icons.arrow_back_ios_outlined,
+                  Icons.share,
                   color: Colors.white,
                 ),
               ),
             ),
           ),
-        ),
-        Positioned(
-          top: 15,
-          right: 15,
-          child: Container(
-            width: 45,
-            height: 45,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Colors.blue[200],
-            ),
-            child: Center(
-              child: Icon(
-                Icons.share,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          bottom: 70,
-          left: 15,
-          child: Container(
-            width: 200,
-            child: Text(
-              dest.destName,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 33,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          bottom: 30,
-          left: 15,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.baseline,
-            textBaseline: TextBaseline.alphabetic,
-            children: [
-              Text(
-                '\$' + dest.destPrice.toStringAsFixed(0),
+          Positioned(
+            bottom: 70,
+            left: 15,
+            child: Container(
+              width: 200,
+              child: Text(
+                dest.destName,
                 style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 25,
                   fontWeight: FontWeight.bold,
+                  fontSize: 33,
+                  color: Colors.white,
                 ),
               ),
-              Text(
-                '/person',
-                style: TextStyle(color: Colors.white),
-              ),
-            ],
-          ),
-        ),
-        Positioned(
-          bottom: 30,
-          right: 15,
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(18),
-              color: colorGrey.withOpacity(0.7),
             ),
-            width: 85,
-            height: 45,
+          ),
+          Positioned(
+            bottom: 30,
+            left: 15,
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.baseline,
+              textBaseline: TextBaseline.alphabetic,
               children: [
-                Icon(
-                  Icons.star,
-                  color: Colors.amber,
-                ),
-                SizedBox(
-                  width: 2,
-                ),
                 Text(
-                  '4.9',
+                  '\$' + dest.destPrice.toStringAsFixed(0),
                   style: TextStyle(
                     color: Colors.white,
+                    fontSize: 25,
                     fontWeight: FontWeight.bold,
-                    fontSize: 18,
                   ),
+                ),
+                Text(
+                  '/person',
+                  style: TextStyle(color: Colors.white),
                 ),
               ],
             ),
           ),
-        ),
-      ],
+          Positioned(
+            bottom: 30,
+            right: 15,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(18),
+                color: colorGrey.withOpacity(0.7),
+              ),
+              width: 85,
+              height: 45,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  Text(
+                    '4.9',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
